884cb162979e2cae00a9abda0214ada82653a0ef
zfeldstein <zfeldstein@linux-6kfl.satx.rr.com>
2019-10-28 22:26:27 -0500

    built config generator

--- cli/main.py
+++ cli/main.py
@@ -1,5 +1,7 @@
 import click
-
+import os.path
+import configparser
+from pathlib import Path
 
 
 @click.group()
@@ -33,39 +35,58 @@ import click
     default=None,
     help='Email address for user'
 )
-def main(ctx, user, passwd, ssh_key,  expiration, email):
+def main(ctx, user, passwd, ssh_key,  expiration, email, config="/.ssso"):
+    config= str(Path.home()) + config
     ctx.obj = {
         "user": user,
         "passwd": passwd,
         "ssh_key": ssh_key,
         "expiration": expiration,
-        "email": email
+        "email": email,
+        "config": config
     }
 
 @main.command()
 @click.pass_context
-def add(ctx):
+def add(ctx,zack):
     click.echo("Adding user %s" % ctx.obj['user'])
 
 @main.command()
 @click.pass_context
-def delete():
+def delete(ctx):
     pass
 
 @main.command()
 @click.pass_context
-def list():
+def list(ctx):
     pass
 
 @main.command()
 @click.pass_context
-def info():
+def info(ctx):
     pass
 
 @main.command()
 @click.pass_context
-def update():
+def update(ctx):
     pass
+# Create a ~/.ssso/config and use for auth with server
+@main.command()
+@click.pass_context
+def config(ctx):
+    config_path = ctx.obj["config"] + "/config"
+    if not os.path.exists(config_path):
+        #Create .ssso dir
+        if not os.path.exists(ctx.obj["config"]):
+            os.mkdir(ctx.obj["config"])
+        config = configparser.ConfigParser()
+        config['DEFAULT'] = {
+            "user_name" : click.prompt("Enter ssso Username"),
+            "passwd" : click.prompt("Enter ssso password"),
+            "url" : click.prompt("Enter ssso URL")
+        }
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
 
 if __name__ == '__main__':
     main()
