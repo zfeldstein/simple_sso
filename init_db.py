9eab965d709deac681bcd55ee529c7f428973d59
Zack Feldstein <zackfeldstein@gmail.com>
2019-11-14 22:18:18 -0600

    admin user bootstrap

--- init_db.py
+++ init_db.py
@@ -1,5 +1,14 @@
 from app import db
-from app import models
+from app.models import Users
 
+# Bootstrap admin user
+admin = Users(username='admin')
+admin.hash_password('cubs2020')
+admin.expiration = 90
+admin.is_admin = True
+
+#Create all imported models + admin user
 db.create_all()
+db.session.add(admin)
 db.session.commit()
+
