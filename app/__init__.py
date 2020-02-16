51f9e2259c70519124760ae736ffb088bd528882
Zack Feldstein <zackfeldstein@gmail.com>
2020-02-15 21:39:47 -0600

    Add Jenkinsfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from flask_marshmallow import Marshmallow
@@ -9,17 +9,18 @@ db = SQLAlchemy()
 ma = Marshmallow()
 auth = HTTPBasicAuth()
 
+
+
 # initialization
 def create_app(conf_file='app.config.ProdConfig'):
     app = Flask(__name__)
-    # app.config.from_object(app_config[config_name])
     app.config.from_object(conf_file)
-    app.config.from_pyfile('config.py')
+    # app.config.from_pyfile('config.py')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     ma.init_app(app)
     db.init_app(app)
+
+    # from app.views import
     return app
 
-# extensions
-
-from app import models
--- test/functional/test_users.py
+++ test/functional/test_users.py
@@ -1,12 +1,12 @@
-
-def test_home_page(test_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/' page is requested (GET)
-    THEN check the response is valid
-    """
-    response = test_client.get('/')
-    assert response.status_code == 200
-    # assert b"Welcome to the Flask User Management Example!" in response.data
-    # assert b"Need an account?" in response.data
-    # assert b"Existing user?" in response.data
+#
+# def test_new_user(test_client):
+#     """
+#     GIVEN a Flask application
+#     WHEN the '/' page is requested (GET)
+#     THEN check the response is valid
+#     """
+#     response = test_client.get('/api/users')
+#     assert response.status_code == 201
+#     # assert b"Welcome to the Flask User Management Example!" in response.data
+#     # assert b"Need an account?" in response.data
+#     # assert b"Existing user?" in response.data
